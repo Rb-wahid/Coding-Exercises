d8be67eda9ed49cd8b150e77381d20ee9fa56ffe
Raihan bin wahid <41251289+Rb-wahid@users.noreply.github.com>
2019-07-18 21:33:53 +0600

    Update 05.backAround.java

--- CodingBat/Warmup-1/05.backAround.java
+++ CodingBat/Warmup-1/05.backAround.java
@@ -1,4 +1,6 @@
 public String backAround(String str) {
   
-  return str.substring(str.length()-1)+str.substring(0,str.length())+str.substring(str.length()-1);
+  String last = str.substring(str.length()-1);
+  
+  return last + str + last;
 }
