1230a081eefcbe28d95ef2789e2740414ae98a03
Raihan bin wahid <41251289+Rb-wahid@users.noreply.github.com>
2019-07-22 22:43:50 +0600

    Create 05_1.IsogramChecker.java

---
+++ Exercism/05_1.IsogramChecker.java
@@ -0,0 +1,20 @@
+
+class IsogramChecker {
+
+    boolean isIsogram(String phrase) {
+	  String str = "";
+	  for(int p = 0; p < phrase.length(); p++){       
+	  	if(phrase.charAt(p) == ' ' || phrase.charAt(p) == '-'){
+			    str = phrase.substring(0,p)+phrase.substring(p+1);
+           phrase = str;
+		}
+	}
+  	for(int p = 0; p < (phrase.length()/2) ; p++){
+  		for(int q = 1; q < phrase.length(); q++){
+  			if(phrase.charAt(p) == phrase.charAt(q)) return false;
+                 }
+            }
+      return true;
+    }
+
+}
