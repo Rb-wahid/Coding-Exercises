34c25e2dc62a484e9ce87a31e88186667731769b
Raihan bin wahid <41251289+Rb-wahid@users.noreply.github.com>
2019-07-22 22:37:33 +0600

    Create 04_1.RaindropConverter.java

---
+++ Exercism/04_1.RaindropConverter.java
@@ -0,0 +1,15 @@
+//( Iteration 2 of 2 )
+class RaindropConverter {
+
+    String convert(int number) {
+       String sound = "";
+	
+	if(number % 3 == 0) sound += "Pling";
+	if(number % 5 == 0) sound += "Plang";
+	if(number % 7 == 0) sound += "Plong";
+
+	if(sound.isEmpty()) sound = Integer.toString(number);
+
+   	return sound;
+    }
+}
